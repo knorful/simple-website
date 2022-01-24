7f6574d13003cf8a021661e4dfd2e31a446d594c
Kris Norful <krisnorful@gmail.com>
2022-01-24 15:56:23 -0600

    complete simple website

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const switcher = document.querySelector('.btn');
+console.log(switcher)
+switcher.addEventListener('click', () => {
+    document.body.classList.toggle('dark-theme')
+
+    var className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
